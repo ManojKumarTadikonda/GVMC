0da1dd87f9906ad34b266cbc3a86c00d0ab4da61
ManojKumarTadikonda <manojtadikonda5@gmail.com>
2024-12-22 11:19:11 +0530

    username

--- controllers/authController.js
+++ controllers/authController.js
@@ -36,7 +36,7 @@ const login = async (req, res) => {
     if (!isMatch) return res.status(400).json({ message: 'Invalid username or password.' });
 
     const token = jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET, { expiresIn: '1h' });
-    res.status(200).json({ token, role: user.role });
+    res.status(200).json({ token, role: user.role, username });
   } catch (err) {
     res.status(500).json({ message: 'Error during login', error: err.message });
   }
